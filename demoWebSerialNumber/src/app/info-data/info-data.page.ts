f69a7202ae76363a2fcb4f15050fe8940bd6c416
BosSaksit <saksit.kku@gmail.com>
2019-07-29 23:59:04 +0700

    validetor homepage

---
+++ demoWebSerialNumber/src/app/info-data/info-data.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-data',
+  templateUrl: './info-data.page.html',
+  styleUrls: ['./info-data.page.scss'],
+})
+export class InfoDataPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
